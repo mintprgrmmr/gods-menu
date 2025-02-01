2e60b5f45e55d2aa3b0fbc356d8c17fd743af026
Mint <mintprgrmmr@gmail.com>
2025-02-01 23:26:06 +0300

    Home and Character Selection

---
+++ resources/views/characterselection.blade.php
@@ -0,0 +1,10 @@
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Выбор персонажа</title>
+</head>
+<body>
+    <h1>Выберите персонажа:</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CharacterSelectionController;
 
-Route::view('/', 'welcome');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
+Route::get('/characters', [CharacterSelectionController::class, 'index'])->name('characters');
